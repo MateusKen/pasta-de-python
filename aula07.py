55e6281963bfd91a6a79094070eaf8e4fbc48446
MateusKen <54600427+MateusKen@users.noreply.github.com>
2019-09-24 17:09:34 -0300

    Add files via upload

---
+++ aula07.py
@@ -0,0 +1,52 @@
+#  (2)
+#nu = float(input("digite qualquer número    "))
+#n = 0
+#if nu > 0:
+	#while n <= nu:
+		#print(n)
+		#n = n + 1
+#elif nu < 0 and nu != 0:
+	#while n >= nu:
+		#print(n)
+		#n = n - 1
+#else:
+	#print(n)
+
+#  (3)
+#nu = float(input("digite um número    "))
+#n = 1
+#while n <= 10:
+	#print(nu*n)
+	#n = n + 1
+
+
+#  (4)
+#import random
+#n = random.randint(1,10)
+#print("adivinhe em que número eu esotu pensando (1 a 10)	")
+#np = int(input())
+#tries = 0
+#while np != n:
+	#input(np)
+	#tries += 1
+#print(tries)
+
+
+#  (5?)
+#import math
+#ni = int(input("digite um número.		"))
+#print(math.factorial(ni))
+
+
+#  (5(true))
+#print("digite um número para se tornar fatorial")
+#print("lembrando que todos os fatoriais são naturais.")	
+#ni = int(input("Digite um número.	")) 
+#n = ni - 1
+#if ni == 0:
+	#print("1")
+#while n != 0:
+	#ni *= n
+	#n -= 1
+#print(ni)
+
