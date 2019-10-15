8748ca8b5ea5a986ecd851e1a757fa8845207a31
MateusKen <54600427+MateusKen@users.noreply.github.com>
2019-10-15 17:08:52 -0300

    Add files via upload

---
+++ aula08.py
@@ -0,0 +1,40 @@
+#frase = "Que dia lindo!!!"
+# letra = input()
+# if letra.lower() == "s":
+# 	print("sair")
+
+
+#print(frase.lower())
+#print(frase.upper())
+#print(frase.title())
+#print(frase.replace(" ", "-"))
+#print(len(frase))
+#print(frase[0])
+#print(frase[1])
+#print(frase[2])
+#print(frase[3])
+#print(frase[-1])
+#print(frase[0])
+#print(frase[::-1])
+
+if "A" < "a":
+	print("verdadeiro")
+else:
+	print("falso")
+
+print(ord("a"))
+print(ord("A"))
+print(chr(1))
+print(chr(2))
+print(chr(3))
+
+pa = "apartamento"
+
+for i in pa:
+	ca = ord(i)
+	nc = ca + 2
+	ncc = chr(nc)
+	np += ncc
+
+print(np)
+
