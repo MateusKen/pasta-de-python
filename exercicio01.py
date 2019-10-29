8cdcd79086a3a29575c54260a9881929e73e4203
MateusKen <54600427+MateusKen@users.noreply.github.com>
2019-10-29 17:04:26 -0300

    Update exercicio01.py

--- exercicio01.py
+++ exercicio01.py
@@ -4,8 +4,82 @@
 #	print(i)
 
 #(2)
-palavra = input("digite uma palavra	")
-for i in palavra:
-	nl = "i"
-	np += nl
-print(np)
+#np = ""
+#palavra = input("digite uma palavra	")
+#for i in palavra:
+#	np += i
+#print(np)
+
+#(3)
+#np = "" 
+#palavra = input("digite uma palavra	")
+#for cont, i in enumerate(palavra):
+#	if cont % 2 == 0:
+#		i = i.lower()
+#	if cont % 2 != 0:
+#		i = i.upper()
+#	np += i
+#	print(i), print(cont)
+#print(np)
+
+#(4)
+#pb = ""
+#espaco = " "
+#palavra = input("digite uma palavra	")
+#for i in palavra:
+#	pb += i
+#	pb += espaco
+#print(pb)
+
+#(5)
+#pb = ""
+#print(ord("a"))
+#print(ord("e"))
+#print(ord("l"))
+#print(ord("t"))
+#palavra = input("digite uma palavra	")
+#for i in palavra:
+#	if ord(i) == 97:
+#		i = "4"
+#	if ord(i) == 101:
+#		i = "3"
+#	if ord(i) == 108:
+#		i = "1"
+#	if ord(i) == 116:
+#		i = "7"
+#	pb += i
+#print(pb)
+
+#(6)
+#pb = ""
+#palavra = input("digite uma palavra	")
+#for i in palavra:
+#	pb += i
+#print(pb[::-1])
+
+#(7)
+#pb = ""
+#palavra = input("digite uma palavra	")
+#for i in palavra:
+#	pb += i
+#if pb == pb[::-1]:
+#	print("Parabéns você digitou um palíndromo")
+#else:
+#	print("Que pena você não digitou um palíndromo :(")
+
+#(8)
+#print("Super verificação de texto:")
+#print("digite um texto,")
+#print("e então digite uma palvra,")
+#print("o programa irá dizer se a palavra aparece no texto")
+#txt = (input("""Digite um texto	"""))
+#word = (input("Digite uma palavra	"))
+#if word in txt:
+#	print("a palavra aparece no texto")
+#else:
+#	print("a palavra não aparece no texto")
+
+#(9)
+#print("Digite uma sentença que pode conter tanto letras, números e carácteres especiais")
+#palavra = input()
+
