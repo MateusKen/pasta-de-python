a5e7ad8d5867dddba7b1e02200183193be50cf83
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-04-19 10:41:14 -0300

    Update exerciciofacul6.py

--- exerciciofacul6.py
+++ exerciciofacul6.py
@@ -3,29 +3,21 @@
 #Caso o tamanho de N seja menor ou igual a zero escrever a mensagem de erro: “Tamanho inválido!”. Obs.: A mensagem de erro  deve ser igual ao informado. 
 #Então, copie e cole esse texto para evitar erro de digitação, ok!
 N = int(input())
-cont = 0
-while cont < N:
-    nro = float(input())
-    if cont == 0: 
-        menor = nro
-        maior = nro
-    else:
-        if nro < menor:
-            menor = nro
-        elif nro > maior:
-            maior = nro
-    cont+=1
-
-if N > 0:
-    print(f"{menor}\n{maior}")
-else:
-    print("Tamanho inválido!")
-
-    
-N = int(input())
-i2 = 0
+i = 0
 if N>0:
-    while i2 <= i:
-        
+    while i < N:
+        num = float(input())
+        if i == 0:
+            menor = num
+            maior = num
+        else:
+            if num < menor:
+                menor = num
+            elif num > maior:
+                maior = num
+        i = i+1
+    print(menor)
+    print(maior)
 else:
     print('Tamanho inválido!')
+
