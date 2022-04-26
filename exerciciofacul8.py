870e8be012a0e32121137ed7acc9c477df794abb
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-04-26 10:53:18 -0300

    Update exerciciofacul8.py

--- exerciciofacul8.py
+++ exerciciofacul8.py
@@ -3,3 +3,13 @@
 #         S = 1/N + 2/N-1 + 3/N-2 + ... + N-1/2 + N
  
 #    Em que N  é fornecido pelo usuário.
+
+s = 0
+
+n = int(input())
+
+for i in range(1,n+1,1):
+    
+    s += i / (n - i + 1)
+    
+print(s)
