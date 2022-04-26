8ba0b42cd4a3b83e6ff6f52bc5bdf71e5e31f51b
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-04-26 10:48:06 -0300

    Create exerciciofacul8.py

---
+++ exerciciofacul8.py
@@ -0,0 +1,6 @@
+#Elabore um programa que calcule e apresente o valor de S, dado por:
+
+#S espaço igual a espaço 1 sobre N mais numerador 2 sobre denominador N menos 1 fim da fração mais numerador 3 sobre denominador N menos 2 fim da fração mais... 
+#mais numerador N menos 1 sobre denominador 2 fim da fração mais N
+ 
+#Em que N  é fornecido pelo usuário.
