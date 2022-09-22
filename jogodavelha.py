b33af99e3abf647b113848ff85096fe29c4eaea7
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-09-22 09:32:41 -0300

    Update jogodavelha.py

--- jogodavelha.py
+++ jogodavelha.py
@@ -2,32 +2,43 @@ def geraTabuleiro():
     M = []
     linha = []
     for i in range(3):
-        linha.append(' . ')
+        linha = []
+        for j in range(3):
+            linha.append(' . ')
         M.append(linha)
     return M
 
-def imprimeTabuleiro(M):
-    for i in range(len(M)):
-        for j in range(len(M[0])):
-            print(M[i][j], end='')
+def imprimeTabuleiro(tabuleiro):
+    for i in range(len(tabuleiro)):
+        for j in range(len(tabuleiro[0])):
+            print(tabuleiro[i][j], end='')
         print('\n')
+    return tabuleiro
 
 def jogar(tabuleiro, jogador, linha, coluna):
-    if jogador == 1:
+    while jogador == 1:
         if tabuleiro[linha-1][coluna-1] == ' . ':
             tabuleiro[linha-1][coluna-1] = ' X '
-            return tabuleiro, True
+            return tabuleiro
         else:
-            print('Jogada Inválida')
-            return tabuleiro, False
-    else:
+            print('Posição Inválida')
+            linha = int(input('Linha: '))
+            coluna = int(input('Coluna: '))
+            print('\n')
+
+
+    while jogador == 2:
         if tabuleiro[linha-1][coluna-1] == ' . ':
             tabuleiro[linha-1][coluna-1] = ' O '
-            return tabuleiro, True
+            return tabuleiro
         else:
-            print('Jogada Inválida')
-            return tabuleiro, False 
-    
+            print('Posição Inválida')
+            linha = int(input('Linha: '))
+            coluna = int(input('Coluna: '))
+            print('\n')
+            
+def verificaGanhador(tabuleiro, jogador):    
+            
 def main():
     flag = False
     jogada = 0
@@ -35,14 +46,16 @@ def main():
     while flag == False:
         imprimeTabuleiro(tabuleiro)
         if jogada%2 == 0:
-            jogador = 1
             jogAL = int(input('Linha: '))
             jogAC = int(input('Coluna: '))
+            print('\n')
+            jogador = 1
             jogar(tabuleiro, jogador, jogAL, jogAC)
-        else:
-            jogador = 2
+        else: 
             jogBL = int(input('Linha: '))
             jogBC = int(input('Coluna: '))
+            print('\n')
+            jogador = 2
             jogar(tabuleiro, jogador, jogBL, jogBC)
         jogada += 1
 
