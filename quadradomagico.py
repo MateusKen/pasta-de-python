c0a3c4bca2597987e80c077d1aa55fb974325346
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-09-23 10:53:12 -0300

    Update quadradomagico.py

--- quadradomagico.py
+++ quadradomagico.py
@@ -19,5 +19,26 @@ def imprimeMatriz(M):
             print(' ', M[i][j],' ', end='')
 
         print('\n')
+
+def verificaQuadrado(M):
+    somaLinha = 0
+    somaColuna = 0
+    i = 0
+    while i < len(M):
+        j = 0
+        while j < len(M):
+            somaLinha = somaLinha+M[i][j]
+            j += 1
+        i +=1
+    if soma  
 arquivo = leArquivo('quadradomagico.txt')
 imprimeMatriz(arquivo)
+arquivo.close()
+
+'''
+EXEMPLO
+3  TAMANHO DO QUADRADO
+8 1 6 1ª LINHA DO QUADRADO
+3 5 7 2ª LINHA DO QUADRADO
+4 9 2 3ª LINHA DO QUADRADO
+'''
